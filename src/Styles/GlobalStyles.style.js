cbef654797c926542cd2dee5e281b4fdd305b7d5
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-25 21:28:47 +0600

    added global styles

---
+++ src/Styles/GlobalStyles.style.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    :root {
+        --clr-bg: #FFFDF3;
+        --clr-text: #F8533A;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        min-height: 100vh;   
+        background-color: var(--clr-bg); 
+    }
+`;
+
+export default GlobalStyles;
