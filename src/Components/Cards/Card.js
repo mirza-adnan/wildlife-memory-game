b2c3bf7ac87540401fbf7028c1aeb6fe1647a286
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-26 21:54:10 +0600

    designed the card

---
+++ src/Components/Cards/Card.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Card() {
+	return (
+		<CardItem>
+			<img src='' alt='' />
+		</CardItem>
+	);
+}
+
+const CardItem = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 2rem;
+	border-radius: 10px;
+	background-color: var(--clr-bg);
+	box-shadow: 0 4px 15px 0px rgb(0 0 0 / 20%);
+	transition: transform 0.2s ease-in-out;
+	cursor: pointer;
+
+	&:hover {
+		transform: scale(1.1);
+	}
+
+	&:active {
+		transform: scale(0.9);
+	}
+
+	& img {
+		width: 100%;
+		max-width: 80px;
+	}
+
+	@media (max-width: 470px) {
+		padding: 1.5rem;
+	}
+`;
+
+export default Card;
