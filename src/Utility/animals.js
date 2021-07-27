4de68fe2e0ad25a33d69006c21081c7bb7f6584b
mirza-adnan <mirza.adnan2205@gmail.com>
2021-07-27 22:29:06 +0600

    created file to store the animal objects

---
+++ src/Utility/animals.js
@@ -0,0 +1,29 @@
+import bee from '../Images/bee.png';
+import bird from '../Images/bird.png';
+import cat from '../Images/cat.png';
+import dog from '../Images/dog.png';
+import elephant from '../Images/elephant.png';
+import koala from '../Images/koala.png';
+import lion from '../Images/lion.png';
+import monkey from '../Images/monkey.png';
+import octopus from '../Images/octopus.png';
+import penguin from '../Images/penguin.png';
+import turtle from '../Images/turtle.png';
+import whale from '../Images/whale.png';
+
+const animalsArray = [
+	{ name: 'Bee', image: bee },
+	{ name: 'Bird', image: bird },
+	{ name: 'Cat', image: cat },
+	{ name: 'Dog', image: dog },
+	{ name: 'Elephant', image: elephant },
+	{ name: 'Koala', image: koala },
+	{ name: 'Lion', image: lion },
+	{ name: 'Monkey', image: monkey },
+	{ name: 'Octopus', image: octopus },
+	{ name: 'Penguin', image: penguin },
+	{ name: 'Turtle', image: turtle },
+	{ name: 'Whale', image: whale },
+];
+
+export default animalsArray;
